7d5047e91a9d674f7d1fba4bde23c68e8c7297e2
Amin7775 <amin.dpi24@gmail.com>
2023-08-20 11:37:24 +0600

    Total Price of Cart Completed

--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,3 @@
-
-
 function setCartList(CardId){
 
     function card(CardId){
@@ -14,4 +12,28 @@ function setCartList(CardId){
     p.innerHTML = `${count}. ${card(CardId)} `;
 
     cartList.appendChild(p);
+}
+
+function getPrice(product){
+    const priceText = document.getElementById(product).innerText;
+    const priceParseValue = parseFloat(priceText);
+    totalPrice(priceParseValue)
+}
+
+function totalPrice(price){
+    const totalPriceValueInnerText = document.getElementById('total-price-text')
+    const totalPriceValueText = totalPriceValueInnerText.innerText ;
+    
+    const totalPriceValueParseFloat = parseFloat(totalPriceValueText);
+ 
+    const previousValue = totalPriceValueParseFloat;
+    
+    const newAddedValue = price;
+    
+    let totalPriceValue = previousValue + newAddedValue;
+
+    totalPriceValue = totalPriceValue.toFixed(2);
+    
+    totalPriceValueInnerText.innerText = totalPriceValue;
+    console.log(totalPriceValueText)
 }
